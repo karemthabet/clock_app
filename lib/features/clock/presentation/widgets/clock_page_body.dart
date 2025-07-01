0d34143d5bd3d3fef8ba1c97e4c058b4b9bd5721
Kareem <karemthabet>
2025-07-01 20:51:13 +0300

    ui done

--- lib/features/clock/presentation/widgets/clock_page_body.dart
+++ lib/features/clock/presentation/widgets/clock_page_body.dart
@@ -1,10 +1,20 @@
+import 'package:clock_app/core/utils/colors/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:one_clock/one_clock.dart';
 
 class ClockPageBody extends StatelessWidget {
   const ClockPageBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return Center(
+      child: AnalogClock(
+        width: 300.w,
+        hourHandColor: AppColors.blackColor,
+        minuteHandColor: AppColors.blackColor,
+        secondHandColor: AppColors.blackColor,
+      ),
+    );
   }
 }
